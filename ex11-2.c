01658199e59e5c6793ac801bed140906d073849d
gimkan1 <gimkan1@gmail.com>
2022-01-10 08:17:31 +0900

    child first

---
+++ ex11-2.c
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 512
+    
+// 자식 저장소
+
+int main()
+{
+    char msg[SIZE];
+    int filedes[2];
+    int i;
+
+    if(pipe(filedes) == -1)
+    {
+        printf("fail to call pipe()\n");
+        exit(1);
+    }
+
+    for(i = 0; i < 3; i++)
+    {
+        printf("input a message (%d)\n", i);
+        fgets(msg, SIZE, stdin);
+        write(filedes[1], msg, SIZE);
+    }
+
+    printf("\n");
+    for(i = 0; i < 3; i++)
+    {
+        read(filedes[0], msg, SIZE);
+        printf("%s", msg);
+    }
+    return 0;
+}
